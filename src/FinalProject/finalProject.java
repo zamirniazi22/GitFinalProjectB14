9e045f3f0daf84fbe11d1a413520f7de83cd2dde
zniazi <zniazi265@gmail.com>
2022-09-28 20:10:58 -0400

    first commit

---
+++ src/FinalProject/finalProject.java
@@ -0,0 +1,14 @@
+package FinalProject;
+
+public class finalProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(("this is the code for mercedes"));
+		
+		
+		
+	}
+
+}
