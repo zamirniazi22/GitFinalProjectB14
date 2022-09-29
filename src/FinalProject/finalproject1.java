22de00afc9c65fa986dddf77e30e582c90740b0c
zniazi <zniazi265@gmail.com>
2022-09-28 20:48:20 -0400

    second commit

---
+++ src/FinalProject/finalproject1.java
@@ -0,0 +1,10 @@
+package FinalProject;
+
+public class finalproject1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" this code is for ausii");
+	}
+
+}
